7d7ebc8513120ebf13a6e582ac8a63635087e3c4
ibhayb <ibrahim.hayber@web.de>
2025-05-14 17:02:30 +0200

    projectwidget remove on hover

--- src/components/ProjectWidget.vue
+++ src/components/ProjectWidget.vue
@@ -6,7 +6,7 @@
     >
       <!-- Animated border layer, visible only on hover -->
       <div
-        class="absolute inset-0 h-full w-full rounded-full bg-[conic-gradient(#3F51B5_20deg,transparent_120deg)] opacity-0 transition-opacity duration-300 group-hover:opacity-100 animate-rotate pointer-events-none"
+        class="absolute inset-0 h-full w-full rounded-full bg-[conic-gradient(#3F51B5_20deg,transparent_120deg)] transition-opacity duration-300 animate-rotate pointer-events-none"
       ></div>
       <div class="z-20 w-full rounded-[0.10rem] bg-black p-8 h-[300px]">
         <h3 class="mb-8 text-[1.5rem]">{{ title }}</h3>
