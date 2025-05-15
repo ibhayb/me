97a2de229f744d40b5e40eeeb9da3ca39aa40047
ibhayb <ibrahim.hayber@web.de>
2025-05-15 10:57:45 +0200

    fix button click area

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -6,11 +6,11 @@ const props = defineProps({
 </script>
 
 <template>
-  <button
-    class="hidden md:block lg:block border border-transparent transform hover:scale-105 ease-in-out transition duration-200 hover:border-slate-200 bg-indigo-600 text-gray-800 font-bold mb-8 py-2 px-4 rounded inline-flex items-center mt-10 lg:mb-20"
-  >
-    <span
-      ><a :href="ahref"> {{ buttonText }}</a></span
+  <a :href="ahref">
+    <button
+      class="hidden md:block lg:block border border-transparent transform hover:scale-105 ease-in-out transition duration-200 hover:border-slate-200 bg-indigo-600 text-gray-800 font-bold mb-8 py-2 px-4 rounded inline-flex items-center mt-10 lg:mb-20"
     >
-  </button>
+      <span> {{ buttonText }}</span>
+    </button>
+  </a>
 </template>
